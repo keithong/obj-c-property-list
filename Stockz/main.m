08e8162e15fa1eb7cbeaf2442133ae1be1d9f9b2
Keith Samson <k.samson@klab.com>
2014-06-05 13:47:22 +0800

    Stockz's first commit

---
+++ Stockz/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Stockz
+//
+//  Created by Keith Samson on 6/5/14.
+//  Copyright (c) 2014 Keith Samson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Property List in Objective-C!");
+        
+    }
+    return 0;
+}
+
